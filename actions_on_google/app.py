30de84c56a7549ececbaea0dbfaeb0924d67e7d4
Simone Locci <simone.locci@cloudesire.com>
2019-08-10 17:51:52 +0200

    Add sentry integration

--- actions_on_google/app.py
+++ actions_on_google/app.py
@@ -1,7 +1,12 @@
+import sentry_sdk
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 
-from actions_on_google.config.config import ENVIRONMENT, PORT, SSL_CONTEXT_CERT, SSL_CONTEXT_KEY
+from actions_on_google.config.config import ENVIRONMENT, PORT, SSL_CONTEXT_CERT, SSL_CONTEXT_KEY, SENTRY_DSN
 from actions_on_google.controllers.ilifev7s import ilifev7s_api
+
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[FlaskIntegration()])
 
 app = Flask(__name__)
 
--- actions_on_google/config/config.py
+++ actions_on_google/config/config.py
@@ -3,6 +3,7 @@ ENVIRONMENT = 'test'  # values allowed are test or prod
 PORT = 4043
 SSL_CONTEXT_CERT = '/etc/letsencrypt/live/my.awesome.domain/fullchain.pem'
 SSL_CONTEXT_KEY = '/etc/letsencrypt/live/my.awesome.domain/privkey.pem'
+SENTRY_DSN = ''
 
 # Ilife v7s zone
 ILIFEV7S_ALLOWED_ACTIONS = ['wake_up', 'clean', 'stop', 'up', 'down', 'left', 'right', 'spot', 'home', 'edge']
